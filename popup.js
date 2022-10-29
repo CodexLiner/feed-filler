0ca7e923ed0803ea71459094cf051580d375a13a
Gopal Meena <meenagopal24@gmail.com>
2022-10-29 18:58:33 +0530

    initial commit

--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const textArea = document.getElementById('feeds');
-    textArea.value = "ssssssssssssssssssssssssssss"
     chrome.storage.local.get(['feed'], function(result) {
-        if(result){
+        if(result.feed){
             textArea.value = result.feed;
         }
     });
+    
     const fillButtton = document.getElementById('fillButton');
     fillButtton.addEventListener('click' , ()=>{
        
         chrome.storage.local.set({"feed": textArea.value}, function() {
-            console.log('Value is set to ' + textArea.value);
+            
             chrome.tabs.getSelected(null, function(tab) {
                 if (tab && tab.status == "complete") {
                     chrome.tabs.executeScript(tab.id, { file: "jquery.js" }, function() {
@@ -26,6 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     })
     document.getElementById('clearButton').addEventListener("click" , ()=>{
+        textArea.value = ''
         chrome.storage.local.set({"feed": ''}, function() { });
     })
 
